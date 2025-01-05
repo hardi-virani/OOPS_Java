53b34a8e505bfb94dfa30e27d7f664354cea864b
Hardi <hardivirani2003@gmail.com>
2025-01-05 14:35:24 -0500

    Checking

--- src/Testing.java
+++ src/Testing.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Testing {
+public class Testing
+{
+    public static void main(String[]args)
+    {
+        //Just checking if Read.Me works
+        System.out.println("Hello");
+    }
 }
