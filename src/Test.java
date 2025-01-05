0a954b661999be58921ef4a58f71a0fcc1c91c97
Hardi <hardivirani2003@gmail.com>
2025-01-05 18:28:31 -0500

    Finding_Errors

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+//Signature same then override.
+public class Test
+{
+    int x;
+
+    //Default Constructor
+    public Test()
+    {
+        x=2;
+    }
+
+    //Parametrised Constructor
+    public Test(String t)
+    {
+        System.out.println("Test");
+    }
+
+    public static void main(String[] args)
+    {
+        Test test = new Test();
+        System.out.println(test.x);
+
+        Test test2 = new Test("Hardi");
+        System.out.println(test2.x);
+    }
+}
