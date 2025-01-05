a99454bd84e8d2d8a64a97bef57943ff1c87e77d
Hardi <hardivirani2003@gmail.com>
2025-01-05 14:45:53 -0500

    Checking

--- src/Testing.java
+++ src/Testing.java
@@ -2,7 +2,7 @@ public class Testing
 {
     public static void main(String[]args)
     {
-        //Just checking if Read.Me works
+        //Just checking if Read.
         System.out.println("Hello");
     }
 }
