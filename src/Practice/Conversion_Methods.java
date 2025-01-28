9167b5a8eab6fe3f6e5bf4b07208f23c32381c9d
Hardi <hardivirani2003@gmail.com>
2025-01-28 13:12:09 -0500

    Conversion_Methods

---
+++ src/Practice/Conversion_Methods.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Conversion_Methods
+{
+    public static void main(String[]args)
+    {
+        Scanner obj = new Scanner(System.in);
+
+
+        System.out.println("Enter any number: ");
+        int n = obj.nextInt();
+    }
+
+}
